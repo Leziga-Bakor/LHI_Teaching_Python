842640ce6ea4907ff169b450c16ac46937a77acd
Leziga Bakor <leziga.bakor@gmail.com>
2023-11-23 10:07:28 +0000

    updated teaching file

--- day_2p.py
+++ day_2p.py
@@ -1,15 +1,21 @@
 
-print 
 
+# loop
+for num in range(100):
+    print(num,end=' ')
 
+# reverse loop
 for num in range(100,0,-5):
     print(num,end=' ')
     
 
 
-# range(start=0,stop=4,step=1)
+'''
+Range syntax
+range(start=0,stop=4,step=1)
 
-# 0
-# 1
-# 2
-# 3
+0
+1
+2
+3 init
+'''
