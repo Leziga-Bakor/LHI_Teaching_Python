187e5f5e35648ad487f39d391c8eaa67b7bea1e3
Leziga Bakor <leziga.bakor@gmail.com>
2023-12-05 19:47:32 +0000

    added third lesson with paul

---
+++ day3_p.py
@@ -0,0 +1,65 @@
+''' 
+Write a program that prints a giant letter A like the one below. Allow the user to specify how
+large the letter should be.
+
+'''
+
+
+
+# int
+# float
+'''
++
+-
+*
+/
+
+5//2=2
+
+21
+
+24
+
+9%3=0
+
+BODMAS = 
+
+b = brackets
+o = power
+d = division
+m = multiplication
+a = addition
+s = subtraction
+
+2**2+2*3+6/2+(1+2)*2
+
+2+2*3+6/2+3*2
+
+
+syntax error
+semantic error
+
+PEMDAS
+
+
+modules
+
+random
+math
+
+
+//
+'''
+
+import random
+from random import randint, seed
+
+num = random.randint(1,10)
+
+num = randint(1,10)
+
+import math
+
+
+
+print(num)
