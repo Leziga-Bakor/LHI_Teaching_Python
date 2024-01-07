c73f00d061b7172b28dde47727ca64972b7558a2
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-07 14:46:45 +0000

    Included topics to be covered

---
+++ LHI/day7.py
@@ -0,0 +1,20 @@
+'''
+LIST
+
+creating list
+- empty list
+- list data types
+
+similarities to String
+- joining(concatenation) and repetition
+- Iterable
+- in operator
+- indexing
+- slicing
+- looping
+
+Mutability
+list methods (replace, sort, append)
+
+
+'''
