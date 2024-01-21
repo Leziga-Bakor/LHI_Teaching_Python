c631176b9c6b7297b41a8bf6979a7443fa0e8e01
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-21 17:12:47 +0000

    Added outlines

---
+++ LHI/day9.py
@@ -0,0 +1,25 @@
+'''
+11. Using a for loop, create the list below, which consists of ones separated by increasingly many
+zeroes. The last two ones in the list should be separated by ten zeroes.
+[1,1,0,1,0,0,1,0,0,0,1,0,0,0,0,1,....]
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Infinite loops 
+The break statement 
+The else statement
+'''
