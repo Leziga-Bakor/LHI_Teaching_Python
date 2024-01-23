02cc831eb136d339996a47acc2057864f01bb65c
Leziga Bakor <leziga.bakor@gmail.com>
2024-01-23 10:38:54 +0000

    Added lecture with tony on strings

--- P/day7.py
+++ P/day7.py
@@ -1,11 +1,59 @@
 ''''
 STRINGS
 '''
+s = 'string'
+s = "string"
 
-# Concatenation and repetition
+# Immutable - means they cannot be modified after creation
+
+# Concatenation and repetition +, *
+s1 = 'This is a man '
+s2 = 'His name is Joseph'
+
+s3 = s1 + s2
+# print(s3)
+
+s = 'a'
+# print(s*10)
+
 # Interable (Indexing and in operator)
+print(s1[2])
+
+# for al in s1:
+#     print(al, end = '---')
+
+if '@' in 'email@email.com':
+    pass
+
+
 # Slices
+s1 = 'This is a man '
+sl = s1[::]
+
+n=11211
+s=str(n)
+n=int(s)
+
 # Changing individual characters of a string
+s = 'string' 
+
+s4 = s[:3] + 'o' + s[4:]
+
 # Looping
-# String methods
+# String methods 
+
+s = s.replace('i','o')
+
+i = 1
+
+print(s4)
+
+s = 4
+j = s
+s = 7 
+
+
+
 # Escape characters \
+
+s="this is london's chief police officer \n"
