60cee0f3ae32d9cd3ee9643c0f241e4acb9b691e
Leziga Bakor <leziga.bakor@gmail.com>
2024-02-04 02:10:18 +0000

    Added subtopics for day 11

---
+++ LHI/day11.py
@@ -0,0 +1,19 @@
+'''
+Tuple
+DICTIONARY - Working with key-value pairs
+
+'''
+
+# Syntax/Declaration
+
+# Dictionary keys
+
+# Setting and updating values
+
+# Removing key-value pairs del,pop
+
+# keys, values, items
+
+# looping
+
+# copying dictionary
