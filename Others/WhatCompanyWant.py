3c9f90c30bf519ff291cb2d86ce693cd95be1283
Leziga Bakor <leziga.bakor@gmail.com>
2024-02-04 02:57:34 +0000

    Added file on what company wants

---
+++ Others/WhatCompanyWant.py
@@ -0,0 +1,9 @@
+'''
+What are Companies looking for 
+
+1. Analytical Skills - How you think through problems
+2. Coding Skills - can you code?
+3. Technical Skills - Do you know the fundamentals
+4. Communication Skills - Can you communicate well with others?
+
+'''
