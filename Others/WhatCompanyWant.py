64471e0c1428e75f3e2e2bc020cc3fd96cf68df6
Leziga Bakor <leziga.bakor@gmail.com>
2024-02-11 19:48:16 +0000

    Updated format by including spaces

--- Others/WhatCompanyWant.py
+++ Others/WhatCompanyWant.py
@@ -2,8 +2,11 @@
 What are Companies looking for 
 
 1. Analytical Skills - How can you think through problems and analyze things?
+
 2. Coding Skills - Do you code well, by writing clean, simple, organized, readable code?
+
 3. Technical Skills - Do you know the fundamentals of the job you're applying for?
+
 4. Communication Skills - Does your personality match the companies’ culture?
 
 '''
