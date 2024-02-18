099750bd3d55e295d3188043c5bf2b467ba08fa1
Leziga Bakor <leziga.bakor@gmail.com>
2024-02-18 20:10:55 +0000

    Added note for set

--- LHI/day12.py
+++ LHI/day12.py
@@ -1,38 +1,50 @@
-# File objects in python
+# # File objects in python
 
-f = open('our_file.txt', 'r')
-f1 = open('new.txt', 'w')
-f1.write('This is the second file\n')
+# f = open('our_file.txt', 'r')
+# f1 = open('new.txt', 'w')
+# f1.write('This is the second file\n')
 
-for line in f:
-    f1.write(line)
+# for line in f:
+#     f1.write(line)
 
-print(file = f1)
-print('I have finished copying. bye', file=f1)
+# print(file = f1)
+# print('I have finished copying. bye', file=f1)
 
-f1.close()
-f.close()
+# f1.close()
+# f.close()
 
-'''
-You are given a file called grades.txt, where each line of the file contains a one-word student
-username and three test scores separated by spaces, like below:.
+# '''
+# You are given a file called grades.txt, where each line of the file contains a one-word student
+# username and three test scores separated by spaces, like below:.
 
-    GWashington 83 77 54
-    JAdams 86 69 90
+#     GWashington 83 77 54
+#     JAdams 86 69 90
 
-Write code that scans through the file and determines how many students passed all three
-tests.
+# Write code that scans through the file and determines how many students passed all three
+# tests.
 
-'''
+# '''
 
 
-# Modes
-# Read -- r
-# write - w
-# append - a
-# read/write  r+
+# # Modes
+# # Read -- r
+# # write - w
+# # append - a
+# # read/write  r+
 
 '''
 SET
 
+list, tuple, dictionary, set
 '''
+
+l = [1,1,2,3,3,4,5,5,5]
+first = set(l)  # {1, 2, 3, 4, 5}
+l = list(first)
+
+second = {1,5,6,7}
+
+print(first | second) # {1, 2, 3, 4, 5, 6, 7}
+print(first & second)  # {1, 5}
+print(first - second)
+print(second - first)
