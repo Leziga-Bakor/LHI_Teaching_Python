ef79b3c29ca82d428386e36cf18f6f4421ef4dd8
Leziga Bakor <leziga.bakor@gmail.com>
2024-02-20 20:36:56 +0000

    File that contains data structure introductory lecture

---
+++ Others/ds.py
@@ -0,0 +1,71 @@
+'''
+
+What is Data Structure?
+
+A data structure is a collection of data
+
+1. Arrays 
+2. Stacks
+3. Queses - 
+4. linked list
+5. Trees
+6.Graphs
+7. Hash tables
+'''
+
+# HOW COMPUTERS STORE DATA
+
+
+# cpu     RAM    ROM
+
+
+'''
+Array
+
+1. insert items
+2. delete
+3. Add
+4. transverse
+5. search
+
+'''
+
+arr = ['a', 'c', 'd', 'e']
+#        0   1    2    3   4
+
+1
+2
+3
+4
+5
+6
+
+
+#Push
+arr.append('e')   #O(1) Armortized constand time
+
+#pop 
+arr.pop()  #O(1)
+
+arr.pop(1)   #O(n)
+
+#insert     #O(n)
+
+# Static vs Dynamic
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+
+print(__name__)
+import myarray
+
