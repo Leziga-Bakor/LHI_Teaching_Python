3cce3672c37811ae54d3aad823b9332ce41fc4c4
Leziga Bakor <leziga.bakor@gmail.com>
2024-03-17 19:33:55 +0000

    Added lecture for the day

---
+++ LHI/day15.py
@@ -0,0 +1,54 @@
+'''
+Scope
+
+LEGB
+
+- Built in Namespace
+- Global Namespace
+- Enclosed Namespace
+- Local Namespace
+
+'''
+
+
+x = 1
+
+def outer():
+    # enclosed Namespace
+    x = 2
+    
+    def inner():
+        # local namespace
+        global x
+        x = 3
+        print('inner', x)
+
+    inner()
+
+    print('outer', x)
+   
+outer()
+
+print('global', x)
+
+
+
+
+
+
+
+
+
+
+
+# x = 2
+
+# def func():
+#     print(x)
+#     x = 3
+
+# func()
+
+
+# x = 2
+
