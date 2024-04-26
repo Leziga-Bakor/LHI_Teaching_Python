85ce09e4cb63841a4ba359716b3a270a37b7d48c
Leziga Bakor <leziga.bakor@gmail.com>
2024-04-26 22:03:33 +0100

    Added except for ZeroDivisionError

--- Nnamdi/errorHandling.py
+++ Nnamdi/errorHandling.py
@@ -6,6 +6,8 @@ try:
     raise ZeroDivisionError('divide')
 except (NameError,ZeroDivisionError) as e:
     print(e)
+except ZeroDivisionError
+    print("Cannot divide by zero")
 except Exception:
     print('there was an error')
 else:
