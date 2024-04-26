48eeecfd6accc1f2d22fa17f4325395ecd01148c
Leziga Bakor <leziga.bakor@gmail.com>
2024-04-26 22:04:11 +0100

    Added mission colon

--- Nnamdi/errorHandling.py
+++ Nnamdi/errorHandling.py
@@ -6,7 +6,7 @@ try:
     raise ZeroDivisionError('divide')
 except (NameError,ZeroDivisionError) as e:
     print(e)
-except ZeroDivisionError
+except ZeroDivisionError:
     print("Cannot divide by zero")
 except Exception:
     print('there was an error')
