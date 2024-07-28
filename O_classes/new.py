2824a2b4e819f71940574fa817f76dfa73ff19ef
Leziga Bakor <leziga.bakor@gmail.com>
2024-07-28 16:20:52 +0100

    class: add folder and first file for python class

---
+++ O_classes/new.py
@@ -0,0 +1,39 @@
+
+# print('HelloWorld','James',123,'stop',sep="--",end="\n")
+# print()
+# print()
+# print('My name is Jude')
+
+
+
+# temp = eval(input('Please enter a number: ')) # eval("7000.0")
+# print(type(temp))
+
+print('**********')
+print('**********')
+print('**********')
+# comment
+
+''' type any paragraph you want here 
+press endter and continue
+
+'''
+
+"""
+Python is dynamically typed
+
+Primary Data types:
+
+Sting: (str) single or double quotation
+
+Integer: (int)
+Float: (float)
+Boolean: (Bool)
+
+"""
+
+our_sting = "They'll have it"
+our_sting = 123  # "123"
+our_sting = 123.0
+our_sting = True
+# print(type(our_sting))
