c011e45fcd3f5185a22074ff38ef08fbc7df09af
Leziga Bakor <leziga.bakor@gmail.com>
2024-08-11 15:14:28 +0100

    Add file for class 2

---
+++ O_classes/class2.py
@@ -0,0 +1,31 @@
+"""
+Loops
+
+- For Loop
+
+- While loop
+
+"""
+#this is a comment
+
+
+
+# DRY - Don't repeat yourself
+
+# for i in range(4):
+#     print('*******')
+
+# print('After for loop')
+
+# Range
+# range(start=0, stop, step=1)
+# 0 1 2 3 --- 4
+
+height = eval(input('Enter a height: '))
+width = eval(input('Enter a width: '))
+
+for i in range(height):
+    print('*'*width)
+
+
+ 
