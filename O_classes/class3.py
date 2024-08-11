83cbad508581303c6b84bcd175c1cb1fd48a3b10
Leziga Bakor <leziga.bakor@gmail.com>
2024-08-11 23:58:59 +0100

    completed class notes for day 3

---
+++ O_classes/class3.py
@@ -0,0 +1,42 @@
+'''
+
+Write a program that uses exactly four for loops to print the sequence of letters below.
+AAAAAAAAAABBBBBBBCDCDCDCDEFFFFFFG
+
+'''
+
+# for i in range(10):
+#     print('A',end='',sep='')
+# for i in range(7):
+#     print('B',end='',sep='')
+# for i in range(4):
+#     print('CD',end='',sep='')
+# print('E',end='',sep='')
+# for i in range(6):
+#     print('F',end='',sep='')
+# print('G',end='',sep='')
+
+"""
+Operators
+
++
+-
+*
+/
+**
+// - integer division
+% - modulo (remainder)
+
+BODMAS
+PEMDAS
+"""
+
+print(5%2)
+
+# print(dir(__builtins__))
+
+
+# modules/packages
+import random
+
+print(random.randint(1,20))
